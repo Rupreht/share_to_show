49946570b2921e00e5c9949cad7273d9c099eeb1
Nikolay Ermolovich <rupreht@rupreht.ru>
2016-03-15 13:59:01 +0500

    Update js.js

--- js.js
+++ js.js
@@ -14,17 +14,3 @@ $(document).ready(function() {
             s.hide(500);
         });
 });
-
-Ya.share2('ya-share', {
-    content: {
-        url: 'http://www.photo-a.ru/portfolio/beline/',
-        title: 'Фотостудия на крыше - рекламная фотосъемка',
-        description: 'Фотосъемка для Билайн',
-        image: 'http://www.photo-a.ru/wa-data/public/photos/30/02/230/230.1080.jpg'   
-    },
-    theme: {
-        services: 'vkontakte,facebook,odnoklassniki,twitter,lj,gplus,evernote,viber,whatsapp',
-        limit: 4,
-    }
-}
-);
